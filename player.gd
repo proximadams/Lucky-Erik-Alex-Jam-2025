be9faf513e7beb00eec5dfbe6b213fcc7a00f612
Lucky Adams <proximadams@gmail.com>
2025-02-21 12:46:34 -0600

    movement bouncing working

--- player.gd
+++ player.gd
@@ -8,19 +8,22 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Angle direction
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	rotation = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	# Handle jump
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		velocity.x = rotation.z * SPEED * -1
+		velocity.z = rotation.x * SPEED
 
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed('restart'):
+		_restart_me()
+
+func _restart_me():
+	velocity = Vector3(0,0,0)
+	position = Vector3(0,3,0)
