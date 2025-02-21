d2648ef772731cc353d09046142a5352b9aa52c2
Lucky Adams <proximadams@gmail.com>
2025-02-21 16:04:56 -0600

    air velocity contributes to next jump velocity

--- Player/player.gd
+++ Player/player.gd
@@ -13,6 +13,7 @@ var jumpConstantSpeedTimer = JUMP_GRACE_WINDOW_TIME
 var jumpEarlyWindowTimer = JUMP_GRACE_WINDOW_TIME
 var secondsSinceOnFloor = 0.0 # airtime?
 var isDiving = false
+var oldAirVelocity = Vector3(0,0,0)
 
 @export var playerID = 0
 var MAX_GROUND_TIME = 0.2
@@ -26,6 +27,7 @@ func _movement(delta: float):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta * GRAVITY_SPEED
+		oldAirVelocity = velocity
 
 	if is_on_floor():
 		$AnimationPlayer.play('SquishJump')
@@ -40,13 +42,13 @@ func _movement(delta: float):
 					numJumpsSoFar += 1
 			didJumpEarly = (jumpEarlyWindowTimer < JUMP_GRACE_WINDOW_TIME)
 			velocity.y = JUMP_VELOCITY
-			velocity.x = (rotation.z * SPEED * -0.7) + (velocity.x * 0.3)
-			velocity.z = (rotation.x * SPEED * 0.7) + (velocity.z * 0.3)
+			velocity.x = (rotation.z * SPEED * -0.7) + (oldAirVelocity.x * 0.3)
+			velocity.z = (rotation.x * SPEED * 0.7) + (oldAirVelocity.z * 0.3)
 
 	elif isDiving: # we are NOT diving i we're on the floor
 		velocity.y = -JUMP_VELOCITY * divePower
-		velocity.x = (rotation.z * SPEED * -0.7) + (velocity.x * 0.3)
-		velocity.z = (rotation.x * SPEED * 0.7) + (velocity.z * 0.3)
+		velocity.x = (rotation.z * SPEED * -0.7) + (oldAirVelocity.x * 0.3)
+		velocity.z = (rotation.x * SPEED * 0.7) + (oldAirVelocity.z * 0.3)
 	secondsSinceOnFloor += delta
 	
 	if jumpConstantSpeedTimer < JUMP_CONSTANT_SPEED_TIME * numJumpsSoFar:
