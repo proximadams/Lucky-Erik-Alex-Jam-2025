6a1fba2e73cfaad6af94c8e669f0a74f066e961c
Lucky Adams <proximadams@gmail.com>
2025-02-23 11:58:27 -0600

    voice counts down "3, 2, 1 FIGHT"

--- UI/count_down.gd
+++ UI/count_down.gd
@@ -1,4 +1,9 @@
 extends CanvasLayer
 
+@export var round = 1
+
+func _ready() -> void:
+	$FightVoiceSounds.get_child(round -1).play()
+
 func set_paused(pauseValue: bool) -> void:
 	get_tree().paused = pauseValue
