d5a0ebb494c85fc9ca2343f020663782b8fa10e4
Lucky Adams <proximadams@gmail.com>
2025-02-23 10:35:36 -0600

    controls shown changes with number of devices connected

--- UI/title_screen.gd
+++ UI/title_screen.gd
@@ -5,3 +5,8 @@ func _ready() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file('res://Environments/Hoodoo/Round1.tscn')
+
+func refresh_controller_instructions():
+	$Controls/ControlsTextureRect0.visible = (Global.numDevicesConnected == 0)
+	$Controls/ControlsTextureRect1.visible = (Global.numDevicesConnected == 1)
+	$Controls/ControlsTextureRect2.visible = (Global.numDevicesConnected == 2)
