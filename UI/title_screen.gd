45141cd7b2504fc252df75b9397150df17f4d1b0
Lucky Adams <proximadams@gmail.com>
2025-02-22 21:05:30 -0600

    plays title screen first then start button takes to Round1

--- UI/title_screen.gd
+++ UI/title_screen.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus()
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://World.tscn")
+	get_tree().change_scene_to_file('res://Environments/Hoodoo/Round1.tscn')
